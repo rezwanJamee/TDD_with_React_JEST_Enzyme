5792be3adfdc0a7e144553ed8cc00597c1c12ed2
rezwanjamee <rezwan.jamee@gmail.com>
2021-10-19 21:57:36 +1100

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Test Driven Development with React JS, Jest and Enzyme
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import Counter from './Counter';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Counter Testing", () => {
+
+  let wrapper;
+  beforeEach(() => {
+    //wrapper = mount(<App />);
+    wrapper = shallow(<Counter />);
+  });
+
+  test('renders the title of counter', () => {
+    expect(wrapper.find('h1').text()).toContain("This is counter react app");
+    // console.log(wrapper.debug());
+  });
+
+  test("render a button with text of `increment`", () => {
+    expect(wrapper.find("#increment-btn").text()).toBe("Increment");
+  });
+
+  test("render the initial value of state in a div", () => {
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  });
+
+  test("render the click event of increment button and increment counter value", () => {
+    wrapper.find("#increment-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+  });
+
+  test("render the click event of decrrment button and decrement counter value", () => {
+    wrapper.find("#increment-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+    wrapper.find("#decrement-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  });
+
 });
